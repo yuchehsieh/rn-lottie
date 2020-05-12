30718fb1d4ebf617d6969a41af2cc73d0646a732
Murphy Hsieh <murphyhsieh@leandev.io>
2020-05-12 08:56:47 +0800

    testing lottie

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LottieView from 'lottie-react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <LottieView source={require('./lottie/Check')} autoPlay loop />
     </View>
   );
 }
